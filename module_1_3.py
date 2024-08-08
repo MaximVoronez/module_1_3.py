91741198053ce873671651a5e8a1742a8f991cc2
MaximVoronez <maxsemerikov2012@mail.ru>
2024-08-08 12:41:11 +0300

    module_1_3.py
    Практическая работа по уроку "Динамическая типизация"

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+Name = 'Дед Мороз'
+print('Name:', Name)
+Age = 100
+print('Age:', Age)
+New_Age = 100 + 50  # Исправлена ошибка, перезаписал значение переменной "New_Age"
+print('New Age:', str(New_Age))
+Is_Student = True
+print('Is_Student:', Is_Student)
